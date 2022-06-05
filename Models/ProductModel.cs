6dc6f7234cec147c4b889dcfcd92ba8d6ebb0475
Victor Vargas <victorvargas@MacBook-Pro-de-Victor.local>
2022-06-05 10:16:39 -0300

    Product model

---
+++ Models/ProductModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Fuel.Models
+{
+    public class ProductModel
+    {
+        public string Name { get; set; }
+        public string Unit { get; set; }
+        public float Price { get; set; }
+        public string Img { get; set; }
+    }
+}
